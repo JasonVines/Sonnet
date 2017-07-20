a8b0990e2bf0b115c2998ffa97f24d549dfd8bf0
JasonVines <mrvines94@gmail.com>
2017-07-20 13:58:11 -0500

    Final Sonnet docs

---
+++ javascripts/sonnet.js
@@ -0,0 +1,20 @@
+"use strict";
+
+console.log("sonnet.js");
+
+
+let sSonnet = document.getElementById('sonnet').innerText;
+
+function myIndex() {
+	let index = sSonnet.indexOf("orphans");
+	console.log("index is", index);
+
+	let length = sSonnet.length;
+	console.log("length =", length);
+
+	/*'gi' is for global and replaces all cases*/
+	let winSwitch = sSonnet.replace("winter", "yuletide").replace(/ the /gi, " a large "); 
+	document.getElementById('sonnet').innerHTML = winSwitch;
+}
+
+myIndex();
